bee4d6a75ad8a810b8286beca96e3df4658dce2f
Dennison Chalacan <134254875+DennisonCc@users.noreply.github.com>
2026-01-21 10:02:22 -0500

    Delete useless directory

--- order-service/src/main/java/ec/edu/espe/order_service/model/OrderStatus.java
+++
@@ -1,8 +0,0 @@
-package ec.edu.espe.order_service.model;
-
-public enum OrderStatus {
-    PENDING,
-    CONFIRMED,
-    CANCELLED,
-    REJECTED
-}
--- order-service/src/main/java/ec/edu/espe/order_service/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package ec.edu.espe.order_service.repository;
-
-import ec.edu.espe.order_service.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderRepository extends JpaRepository<Order, String> {
-}
